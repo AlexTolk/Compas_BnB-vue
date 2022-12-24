112fa25a70ff2da3c7a0246fb1028b07fbc00325
Andras Varga <vargan89@gmail.com>
2022-12-24 16:03:28 +0800

    feat: added profile

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'pages/home'
+  get 'pages/profile'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
