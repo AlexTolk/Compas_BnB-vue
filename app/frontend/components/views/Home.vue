567afb06da5a425d6edfa81ed02027082e9ea9bb
Sneha Sunny <snehasunny1997@gmail.com>
2022-12-27 19:25:07 +0800

    Completed homepage and adjusted footer

--- app/frontend/components/views/Home.vue
+++ app/frontend/components/views/Home.vue
@@ -10,28 +10,3 @@
     </div>
   </div>
 </template>
-<!--
-<script>
-export default {
-  name: 'Home',
-  props: {
-    btnText: { type: String, value: 'Hello' },
-  },
-  data() {
-    return {
-      // Component's initial data
-      // btnText: "hello",
-      dir: "frontend/components/views/Home.vue"
-    }
-  },
-  mounted() {
-    console.log('hello from VUE')
-  },
-  methods: {
-    sayHello() {
-      console.log('You clicked a Vue button 🎉')
-    }
-  }
-}
-};
-</script> -->
