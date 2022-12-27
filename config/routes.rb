fa22aed650e0276a137553494c42c8c544a39ee8
Andras Varga <vargan89@gmail.com>
2022-12-27 22:35:43 +0800

    updated routes and home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  get 'pages/home'
+  root to: 'flats#index'
+  # get 'pages/home'
   get 'pages/profile'
-  get 'flats/show'
-  get 'bookings/show'
+  # get 'flats/show'
+  # get 'bookings/show'
 
   resources :flats
   resources :bookings
