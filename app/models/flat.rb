1b94488f4673fc3aed34552ab0991d4e67f805cb
Aleksandr Tolkachev <alexandr.tolkachyov@gmail.com>
2022-12-31 15:50:36 +0800

    fix: fixed the logout function

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -3,6 +3,31 @@
 # Table name: flats
 #
 #  id           :bigint           not null, primary key
+#  address      :string
+#  availability :date
+#  avg_rating   :float
+#  cancellation :text
+#  city         :string
+#  country      :string
+#  description  :text
+#  details      :text
+#  ground_rules :text
+#  name         :string
+#  price        :float
+#  state        :string
+#  zipcode      :integer
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  user_id      :bigint           not null
+#
+# Indexes
+#
+#  index_flats_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 
 #  address      :string
 #  availability :date
